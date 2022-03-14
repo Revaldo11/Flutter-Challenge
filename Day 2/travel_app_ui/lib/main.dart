d480e20726aa2e2b650b299472955dccf03bdad6
Revaldo11 <revaldoputra11@gmail.com>
2022-03-14 19:15:10 +0700

    Initial

---
+++ Day 2/travel_app_ui/lib/main.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'Nunito'),
+        home: HomePage(),
+      ),
+    );
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late PageController _pageController;
+
+  void _onScroll() {
+    print('Scroll');
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(_onScroll);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        scrollDirection: Axis.vertical,
+        children: [
+          getPage(
+            page: 1,
+            image: 'assets/images/01.jpg',
+            title: 'Rialto Bridge',
+            description:
+                'The Rialto Bridge is the most famous of the bridges that cross the Grand Canal (the other two are the Accademia Bridge and the Scalzi).',
+          ),
+          getPage(
+            page: 2,
+            image: 'assets/images/02.jpg',
+            title: 'Ulun Danu',
+            description:
+                'Pura Ulun Danu Beratan, or Pura Bratan, is a major Hindu Shaivite temple in Bali, Indonesia. The temple complex is on the shores of Lake Bratan in the mountains near Bedugul.',
+          ),
+          getPage(
+            page: 3,
+            image: 'assets/images/03.jpg',
+            title: 'Siargao island',
+            description:
+                'Siargao is a tear-drop shaped island in the Philippine Sea situated 196 kilometers southeast of Tacloban. It has a land area of approximately 437 square kilometres (169 sq mi). The east coast is relatively straight with one deep inlet, Port Pilar.',
+          ),
+          getPage(
+            page: 4,
+            image: 'assets/images/04.jpg',
+            title: 'Hakone',
+            description:
+                'Hakone is a town in Kanagawa Prefecture, Japan. As of 1 April 2021, the town had an estimated population of 10,837 and a population density of 3000 persons per km². The total area of the town is 92.82 km².',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget getPage({image, title, description, page}) {
+  void showToastMessage(String message) {
+    Fluttertoast.showToast(
+        msg: "Readme Button Pressed",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        // timeInSecForIosWeb: 1,
+        // backgroundColor: Colors.,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  int totalPage = 4;
+  return Container(
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        fit: BoxFit.cover,
+        image: AssetImage(image),
+      ),
+    ),
+    child: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomRight,
+          stops: [0.3, 0.9],
+          colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.2),
+          ],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              mainAxisAlignment: MainAxisAlignment.end,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Text(
+                  page.toString(),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '/' + totalPage.toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 3),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                          size: 15,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 5),
+                        child: Icon(
+                          Icons.star,
+                          color: Colors.grey,
+                          size: 15,
+                        ),
+                      ),
+                      Text(
+                        '4.0',
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 45),
+                    child: Text(
+                      description,
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(.7), height: 1.8),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                showToastMessage("Show Toast Message on Flutter");
+              },
+              child: Text(
+                'READ MORE',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
